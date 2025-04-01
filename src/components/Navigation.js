822930b49237df7ddd3666b33908a412b9d36741
Matt Allen <allen.matt@gmail.com>
2025-04-01 11:44:36 -0400

    wip - add network switching logic that updates metamask from UI

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,28 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import logo from '../logo.png';
 import { useSelector, useDispatch } from 'react-redux';
 import Blockies from 'react-blockies';
 import { loadAccount, loadBalances } from '../store/interactions';
+import config from '../config.json';
 
 const Navigation = () => {
 	const account = useSelector((state) => state.provider.account);
 	const tokens = useSelector((state) => state.tokens.contracts);
+	const chainId = useSelector((state) => state.provider.chainId);
 	const dispatch = useDispatch();
 
 	const connectHandler = async () => {
 		const account = await loadAccount(dispatch);
 		await loadBalances(tokens, account, dispatch);
+	};
+
+	const networkHandler = async (e) => {
+		await window.ethereum.request({
+			method: 'wallet_switchEthereumChain',
+			params: [{ chainId: e.target.value }],
+		});
 	};
 
 	return (
@@ -26,22 +36,41 @@ const Navigation = () => {
 			/>
 			<Navbar.Brand href='#'>Dapp University Template</Navbar.Brand>
 			<Navbar.Collapse className='justify-content-end'>
-				{account ? (
-					<Navbar.Text>
-						{account.slice(0, 5) + '...' + account.slice(38, 42)}
-						<Blockies
-							seed={account}
-							size={10}
-							scale={3}
-							color='#2187D0'
-							bgColor='#F1F2F9'
-							spotcolor='#767F92'
-							className='identicon mx-2'
-						></Blockies>
-					</Navbar.Text>
-				) : (
-					<Button onClick={connectHandler}>Connect</Button>
-				)}
+				<div className='d-flex justify-content-end mt-3'>
+					<Form.Select
+						aria-label='Network Selector'
+						value={
+							config[chainId] ? `0x${chainId.toString(16)}` : `0`
+						}
+						onChange={networkHandler}
+						style={{ maxWidth: '200px', marginRight: '20px' }}
+					>
+						<option value='0' disabled>
+							Select Network
+						</option>
+						<option value='0x7A69'>Localhost</option>
+						<option value='0x5'>Goerli</option>
+					</Form.Select>
+
+					{account ? (
+						<Navbar.Text>
+							{account.slice(0, 5) +
+								'...' +
+								account.slice(38, 42)}
+							<Blockies
+								seed={account}
+								size={10}
+								scale={3}
+								color='#2187D0'
+								bgColor='#F1F2F9'
+								spotcolor='#767F92'
+								className='identicon mx-2'
+							></Blockies>
+						</Navbar.Text>
+					) : (
+						<Button onClick={connectHandler}>Connect</Button>
+					)}
+				</div>
 			</Navbar.Collapse>
 		</Navbar>
 	);
