116873b2cd9f9bac222ae770957298c83aefd40a
Matt Allen <allen.matt@gmail.com>
2025-04-21 21:20:09 -0400

    create withdraw form

--- src/components/Withdraw.js
+++ src/components/Withdraw.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Spinner from 'react-bootstrap/Spinner';
+import { loadBalances, addLiquidity } from '../store/interactions';
+import Alert from './Alert';
 
 const Withdraw = () => {
-	return <div>Withdraw</div>;
+	const provider = useSelector((state) => state.provider.connection);
+	const account = useSelector((state) => state.provider.account);
+
+	const withdrawHandler = async (e) => {
+		e.preventDefault();
+		console.log('withdrawhandler...');
+	};
+
+	return (
+		<div>
+			<Card style={{ maxWidth: '450px' }} className='mx-auto px-4'>
+				{account ? (
+					<Form
+						onSubmit={withdrawHandler}
+						style={{ maxWidth: '450px', margin: '50px auto' }}
+					>
+						<Row>
+							<Form.Text className='text-end my-2' muted>
+								Shares: {0}
+							</Form.Text>
+
+							{/* Input values */}
+							<InputGroup>
+								<Form.Control
+									type='number'
+									placeholder='0.0'
+									min='0.0'
+									step='any'
+									id='shares'
+								></Form.Control>
+								<InputGroup.Text
+									style={{ width: '100px' }}
+									className='justify-content-center'
+								>
+									Shares
+								</InputGroup.Text>
+							</InputGroup>
+						</Row>
+						<Row className='my-3'>
+							<Button type='submit'>Withdraw</Button>
+						</Row>
+						<hr />
+						<Row>
+							<p>
+								<strong>DAPP Balance: </strong>0
+							</p>
+							<p>
+								<strong>USD Balance: </strong>0
+							</p>
+						</Row>
+					</Form>
+				) : (
+					<p
+						className='d-flex justify-content-center align-items-center'
+						style={{ height: '300px' }}
+					>
+						Please connect wallet
+					</p>
+				)}
+			</Card>
+		</div>
+	);
 };
 
 export default Withdraw;
